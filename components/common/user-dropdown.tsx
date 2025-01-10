f7d452200c40e6da554b22f37c54baf5b1b8512b
awebcode <asikurrahaman997@gmail.com>
2025-01-10 10:35:10 +0600

    updated

--- components/common/user-dropdown.tsx
+++ components/common/user-dropdown.tsx
@@ -11,16 +11,21 @@ import { signOut } from "next-auth/react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { Role } from "@prisma/client";
+import { useRouter } from "next/navigation";
 interface UserDropdownProps {
   name?: string | null;
-    image?: string | null;
-    role?: Role
+  image?: string | null;
+  role?: Role;
 }
 const UserDropdown: React.FC<UserDropdownProps> = ({ name, image, role }) => {
+  const router = useRouter();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button title={name??"Profile"} className="flex flex-col items-center gap-2 px-4">
+        <button
+          title={name ?? "Profile"}
+          className="flex flex-col items-center gap-2 px-4"
+        >
           <Avatar className="w-8 h-8">
             <AvatarImage src={image || ""} alt={name || "User"} />
             <AvatarFallback>{name?.charAt(0).toUpperCase() || "U"}</AvatarFallback>
@@ -29,14 +34,14 @@ const UserDropdown: React.FC<UserDropdownProps> = ({ name, image, role }) => {
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-44">
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => router.push("/profile")}>
           <Link href="/profile">Profile</Link>
-              </DropdownMenuItem>
-              {role===Role.ADMIN && (
-                <DropdownMenuItem>
-                  <Link href="/admin">Admin</Link>
-                </DropdownMenuItem>
-              )}
+        </DropdownMenuItem>
+        {role === Role.ADMIN && (
+          <DropdownMenuItem onClick={() => router.push("/admin")}>
+            <Link href="/admin">Admin</Link>
+          </DropdownMenuItem>
+        )}
         <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
