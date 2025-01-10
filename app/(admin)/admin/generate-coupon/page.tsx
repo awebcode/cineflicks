80ffa8f339da95774e20dbc710170b45ff2b6203
awebcode <asikurrahaman997@gmail.com>
2025-01-10 12:23:48 +0600

    updated

--- app/(admin)/admin/generate-coupon/page.tsx
+++ app/(admin)/admin/generate-coupon/page.tsx
@@ -228,7 +228,7 @@ const GenerateCouponPage = () => {
           {hasNextPage && (
             <button
               onClick={() => fetchNextPage()}
-              className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+              className="px-4 py-2 w-fit bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
               disabled={isFetchingNextPage}
             >
               {isFetchingNextPage ? "Loading..." : "Load More"}
--- app/(admin)/admin/user-list/page.tsx
+++ app/(admin)/admin/user-list/page.tsx
@@ -78,7 +78,7 @@ const UserListPage = () => {
           {hasNextPage && (
             <button
               onClick={() => fetchNextPage()}
-              className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+              className="px-4 py-2 w-fit bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
               disabled={isFetchingNextPage}
             >
               {isFetchingNextPage ? "Loading..." : "Load More"}
